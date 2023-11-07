33e6473bc8a90a1f2b2400d32cca9cc691aae3cc
maurya-anshul <mauryanshul93@gmail.com>
2023-11-07 18:44:53 +0530

    li changes added

--- Demo.js
+++ Demo.js
@@ -3,3 +3,5 @@ list[2].style.backgroundColor='green';
 for(var i=0;i<list.length;i++){
     list[i].style.fontWeight='bold';
 }
+var li=document.getElementsByTagName('li');
+li[4].style.backgroundColor='yellow';
